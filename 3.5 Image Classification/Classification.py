616d272b4f0ae6839f90fe055423e6dbe8f6dc18
sxx <suxiaoxin2016@163.com>
2022-04-01 13:00:15 +0800

    Classification

---
+++ 3.5 Image Classification/Classification.py
@@ -0,0 +1,216 @@
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from IPython import display
+import torch.nn as nn
+
+display.set_matplotlib_formats('svg')
+
+
+# trains = transforms.ToTensor()
+# mnist_train = torchvision.datasets.FashionMNIST(root="./data", train=True, transform=trains, download=True)
+# mnist_test = torchvision.datasets.FashionMNIST(root="./data", train=False, transform=trains, download=True)
+#
+# print(len(mnist_train))
+# print(len(mnist_test))
+# print(mnist_train[0][0].shape)
+
+
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            ax.imshow(img.numpy())
+        else:
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+
+# X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+# show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
+# plt.show()
+
+
+# train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True)
+# timer = time.time()
+# for X, y in train_iter:
+#     continue
+# print(f'time is : {time.time() - timer}')
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    trans = transforms.ToTensor()
+    # if resize:
+    #     trans.insert(0, transforms.Resize(resize))
+    # trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root='./data', train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root='./data', train=False, transform=trans, download=True)
+    return (
+        data.DataLoader(mnist_train, batch_size, shuffle=True),
+        data.DataLoader(mnist_test, batch_size, shuffle=False))
+
+
+batch_size = 128
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+
+num_inputs = 784
+num_output = 10
+W = torch.normal(0, 0.01, size=(num_inputs, num_output), requires_grad=True)
+b = torch.zeros(num_output, requires_grad=True)
+
+
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition
+
+
+# def net(X):
+#     return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+
+
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+
+
+# print(y_hat[[0, 1], y])
+# print(cross_entropy(y_hat, y))
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+# print(accuracy(y_hat, y) / len(y))
+
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+print(evaluate_accuracy(net, test_iter))
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)  # 训练损失总和, 训练准确度总和, 样本数
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean.backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+class Animator:
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        if legend is None:
+            legend = []
+        display.set_matplotlib_formats('svg')
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        # if nrows * ncols == 1:
+        #     self.axes = [self.axes, ]
+        self.config_axes = lambda: self.axes.set(xlabel=xlabel, ylabel=ylabel, xlim=xlim, ylim=ylim, xscale=xscale,
+                                                 yscale=yscale)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        if not hasattr(y, '__len__'):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, '__len__'):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes.cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes.plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+        plt.show()
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_meatrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_meatrics + (test_acc,))
+    train_loss, train_acc = train_meatrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+
+lr = 0.1
+
+
+def updater(batch_size):
+    return sgd([W, b], lr, batch_size)
+
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
